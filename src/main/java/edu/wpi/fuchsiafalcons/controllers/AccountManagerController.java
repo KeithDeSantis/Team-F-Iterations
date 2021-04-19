e365e0d72465a5761715a870546bd3ed87f82c63
Jay <47358206+jay-oliver@users.noreply.github.com>
2021-04-19 17:04:51 -0400

    update login on default screen

--- src/main/java/edu/wpi/fuchsiafalcons/controllers/AccountManagerController.java
+++ src/main/java/edu/wpi/fuchsiafalcons/controllers/AccountManagerController.java
@@ -1,8 +1,54 @@
 package edu.wpi.fuchsiafalcons.controllers;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 
 public class AccountManagerController {
+    @FXML
+    private JFXButton quit;
+    @FXML
+    private JFXButton deleteUser;
+    @FXML
+    private JFXButton addUser;
+    @FXML
+    private JFXButton saveChanges;
+    @FXML
+    private JFXButton home;
+    @FXML
+    private JFXComboBox name;
+    @FXML
+    private JFXComboBox existingUserType;
+    @FXML
+    private JFXComboBox newUserType;
+    @FXML
+    private JFXTextField username;
+    @FXML
+    private JFXTextField password;
+    @FXML
+    private JFXTextField addName;
+    @FXML
+    private JFXTextField addPassword;
+    @FXML
+    private JFXTextField addUsername;
+
     public void handleUserSearch(ActionEvent actionEvent) {
+    }
+
+    public void handleQuit(ActionEvent actionEvent) {
+    }
+
+    public void handleDeleteUser(ActionEvent actionEvent) {
+    }
+
+    public void handleAddUser(ActionEvent actionEvent) {
+    }
+
+    public void handleSaveChanges(ActionEvent actionEvent) {
+    }
+
+    public void handleAdminHome(ActionEvent actionEvent) {
     }
 }
