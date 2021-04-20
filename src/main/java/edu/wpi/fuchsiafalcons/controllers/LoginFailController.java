e25e85f31a6dd0f42abb5b0872801a07fc6727bc
Jay <47358206+jay-oliver@users.noreply.github.com>
2021-04-20 16:25:55 -0400

    jyen - fixed login close button

--- src/main/java/edu/wpi/fuchsiafalcons/controllers/LoginFailController.java
+++ src/main/java/edu/wpi/fuchsiafalcons/controllers/LoginFailController.java
@@ -3,6 +3,8 @@ package edu.wpi.fuchsiafalcons.controllers;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import edu.wpi.fuchsiafalcons.database.ConnectionHandler;
+import edu.wpi.fuchsiafalcons.database.DatabaseAPI;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -39,11 +41,17 @@ public class LoginFailController {
      * @throws IOException
      * @author Jay Yen
      */
-    public void handleSignIn(ActionEvent actionEvent) throws IOException {
-        //signs in
-        if (/*username and password match an account*/ true){
+    public void handleSignIn(ActionEvent actionEvent) throws Exception {
+        boolean authenticated = false;
+        String user = username.getText();
+        String pass = password.getText();
+
+        DatabaseAPI.getDatabaseAPI().dropTable(ConnectionHandler.getConnection(), "USERS");
+        DatabaseAPI.getDatabaseAPI().populateUsers(ConnectionHandler.getConnection());
+        authenticated = DatabaseAPI.getDatabaseAPI().authenticate(user, pass);
+        if (authenticated){
             Stage currentStage = (Stage)signIn.getScene().getWindow();
-            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/fuchsiafalcons/fxml/DefaultPageView.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/fuchsiafalcons/fxml/DefaultPageAdminView.fxml"));
             Scene homeScene = new Scene(root);
             currentStage.setScene(homeScene);
             currentStage.show();
