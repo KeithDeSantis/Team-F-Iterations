6ce3a91ad7ab0133edba201306acf08ca79fe7fe
Leo Morris <lmmorris@wpi.edu>
2021-04-20 15:07:47 -0400

    LM - Added test for edit edge dialogue and edge entry

---
+++ src/test/java/edu/wpi/fuchsiafalcons/controllers/EditMapEdgeDialogueControllerTest.java
@@ -0,0 +1,39 @@
+package edu.wpi.fuchsiafalcons.controllers;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+import java.io.IOException;
+import java.util.Objects;
+
+import static org.testfx.api.FxAssert.verifyThat;
+
+public class EditMapEdgeDialogueControllerTest extends ApplicationTest{
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/edu/wpi/fuchsiafalcons/fxml/EditMapEdgesView.fxml")));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    @Test
+    public void testIsFilledOut(){
+        clickOn("New...");
+        verifyThat("OK", Node::isVisible);
+        clickOn("OK");
+        verifyThat("OK", Node::isVisible);
+        clickOn("#startNode");
+        write("Test");
+        clickOn("#endNode");
+        write("Edge");
+        clickOn("OK");
+        verifyThat("Test_Edge", Node::isVisible);
+    }
+}
