2f01a836a95fd575e45c54dcb37f152740824731
Tony Vuolo <vuolo.969473@gmail.com>
2021-04-22 19:49:59 -0400

    Tony Vuolo - added approximate zoom feature to Pathfinding

--- src/main/java/edu/wpi/fuchsiafalcons/controllers/AStarDemoController.java
+++ src/main/java/edu/wpi/fuchsiafalcons/controllers/AStarDemoController.java
@@ -7,16 +7,18 @@ import edu.wpi.fuchsiafalcons.pathfinding.Vertex;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
-import javafx.scene.control.ScrollPane;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.input.ScrollEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -32,11 +34,12 @@ import java.util.stream.Collectors;
 public class AStarDemoController implements Initializable {
 
     @FXML
+    public Label titleLabel;
+    @FXML
     private Button goBack;
     @FXML
     private ImageView map;
-    @FXML
-    private ScrollPane scroll;
+
     @FXML
     private Pane canvas;
 
@@ -54,6 +57,8 @@ public class AStarDemoController implements Initializable {
     
     private double zoomLevel = 4.0;
 
+    private double height, width;
+
     /**
      * These are done for displaying the start & end nodes. This should be done better (eventually)
      * @author Alex Friedman (ahf)
@@ -63,15 +68,15 @@ public class AStarDemoController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
 
+
+        map.toBack();
         map.setPreserveRatio(true);
 
         final Image image = new Image(getClass().getResourceAsStream("/maps/01_thefirstfloor.png"));
 
-        final double width = image.getWidth()/zoomLevel;
-        final double height = image.getHeight()/zoomLevel;
+        width = image.getWidth()/zoomLevel;
+        height = image.getHeight()/zoomLevel;
 
         canvas.setPrefSize(width,height);
         map.setFitWidth(width);
@@ -95,9 +100,85 @@ public class AStarDemoController implements Initializable {
         startComboBox.setItems(nodeList);
         endComboBox.setItems(nodeList);
         pathfindButton.setDisable(true);
+        initializeDrag();
     }
 
-        /**
+    private double OFFSET_X = 0, OFFSET_Y = 0;
+
+    /**
+     * Creates all EventHandlers for the drag zoom functionality
+     */
+    public void initializeDrag() {
+        final double MIN_X = -20, MAX_X = 920, MIN_Y = 10, MAX_Y = 485;
+        EventHandler<MouseEvent> pressHandler = pressEvent -> {
+            EventHandler<MouseEvent> dragHandler = dragEvent -> {
+                map.setTranslateX(median(
+                        dragEvent.getSceneX() - pressEvent.getSceneX(),
+                         MIN_X - map.getLayoutX() - OFFSET_X,
+                        -map.getLayoutX() + MAX_X - width - OFFSET_X
+                ));
+
+                map.setTranslateY(median(
+                        dragEvent.getSceneY() - pressEvent.getSceneY(),
+                        MIN_Y - map.getLayoutY() - OFFSET_Y,
+                        -map.getLayoutY() + MAX_Y - height - OFFSET_Y
+                ));
+            };
+            map.addEventFilter(MouseEvent.MOUSE_DRAGGED, dragHandler);
+            EventHandler<MouseEvent> releaseHandler = releaseEvent -> {
+                map.setLayoutX(map.getLayoutX() + map.getTranslateX());
+                map.setLayoutY(map.getLayoutY() + map.getTranslateY());
+                map.setTranslateX(0);
+                map.setTranslateY(0);
+            };
+            map.addEventFilter(MouseEvent.MOUSE_RELEASED, releaseHandler);
+        };
+        map.addEventFilter(MouseEvent.MOUSE_PRESSED, pressHandler);
+
+        final double SENSITIVITY = 0.001;
+        EventHandler<ScrollEvent> scrollHandler = scrollEvent -> {
+            final double ZOOM_FACTOR = median(0.4, 2,map.getScaleX() - scrollEvent.getDeltaY() * SENSITIVITY);
+            final double RATIO = ZOOM_FACTOR / map.getScaleX();
+            map.setScaleX(ZOOM_FACTOR);
+            map.setScaleY(ZOOM_FACTOR);
+            height *= RATIO;
+            width *= RATIO;
+            OFFSET_X -= (width - width / RATIO) / 2;
+            OFFSET_Y -= (height - height / RATIO) / 2;
+
+            final double RADIAL_DISTANCE_X = (OFFSET_X + map.getLayoutX() + width/2 - scrollEvent.getSceneX());
+            final double RADIAL_DISTANCE_Y = (OFFSET_Y + map.getLayoutY() + height/2 - scrollEvent.getSceneY());
+            map.setLayoutX(map.getLayoutX() + (RADIAL_DISTANCE_X - RADIAL_DISTANCE_X / RATIO));
+            map.setLayoutY(map.getLayoutY() + (RADIAL_DISTANCE_Y - RADIAL_DISTANCE_Y / RATIO));
+//            if(RATIO >= 1) {
+//            } else {
+//                map.setLayoutX(median(map.getLayoutX() + (RADIAL_DISTANCE_X - RADIAL_DISTANCE_X / RATIO),
+//                        MIN_X - OFFSET_X, MAX_X - width - OFFSET_X));
+//                map.setLayoutY(median(map.getLayoutY() + (RADIAL_DISTANCE_Y - RADIAL_DISTANCE_Y / RATIO),
+//                        MIN_Y - OFFSET_Y, MAX_Y - height - OFFSET_Y));
+//            }
+
+            System.out.println(
+                    RADIAL_DISTANCE_X
+                    + " " +
+                    RADIAL_DISTANCE_Y
+            );
+        };
+        map.addEventHandler(ScrollEvent.SCROLL, scrollHandler);
+    }
+
+    /**
+     * Finds the median of three values
+     * @param a the first value
+     * @param b the second value
+     * @param c the third value
+     * @return the middle value
+     */
+    public double median(double a, double b, double c) {
+        return a + b + c - Math.max(a, Math.max(b, c)) - Math.min(a, Math.min(b, c));
+    }
+
+    /**
          * Handles the pushing of a button on the screen
          * @param actionEvent the button's push
          * @throws IOException in case of scene switch, if the next fxml scene file cannot be found
@@ -118,30 +199,6 @@ public class AStarDemoController implements Initializable {
             stage.show();
         }
     }
-
-    //Please comment this!
-    private double prevX, prevY;
-    private boolean firstClick = false;
-    private Line newLine;
-    public void drawLine(MouseEvent mouseEvent) {
-        /*
-        if (!mouseEvent.isPrimaryButtonDown()) {
-            return;
-        }
-        firstClick = ! firstClick;
-        if(!firstClick){
-            newLine = new Line(prevX, prevY,
-                    mouseEvent.getX(), mouseEvent.getY());
-            newLine.setStrokeWidth(5);
-            newLine.setStroke(Color.BLACK);
-            canvas.getChildren().add(newLine);
-        }else{
-            prevX = mouseEvent.getX();
-            prevY = mouseEvent.getY();
-        }
-         */
-    }
-
 
     /**
      * Handles the pathfind btn.
