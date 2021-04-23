955448692f926dc52f40de2ab96161f87041b19f
Leo Morris <lmmorris@wpi.edu>
2021-04-23 11:53:48 -0400

    LM - Button FX:ID and actions setup

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/MapEditViewController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/MapEditViewController.java
@@ -1,4 +1,37 @@
 package edu.wpi.cs3733.D21.teamF.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
+
 public class MapEditViewController {
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    public void handleLoad(ActionEvent actionEvent) {
+    }
+
+    public void handleHoverOn(MouseEvent mouseEvent) {
+    }
+
+    public void handleHoverOff(MouseEvent mouseEvent) {
+    }
+
+    public void handleNew(ActionEvent actionEvent) {
+    }
+
+    public void handleEdit(ActionEvent actionEvent) {
+    }
+
+    public void handleDelete(ActionEvent actionEvent) {
+    }
+
+    public void handleHome(MouseEvent mouseEvent) {
+    }
+
+    public void handleSearch(KeyEvent keyEvent) {
+    }
+
+    public void handleReset(ActionEvent actionEvent) {
+    }
 }
