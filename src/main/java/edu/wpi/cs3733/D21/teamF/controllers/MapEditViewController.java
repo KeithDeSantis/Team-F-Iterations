f5161b6f52fdad71a29e7bed653e8d480c7ec925
keithdesantis <kwdesantis@wpi.edu>
2021-04-23 18:41:41 -0400

    KD - working on bugging

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/MapEditViewController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/MapEditViewController.java
@@ -6,6 +6,7 @@ import edu.wpi.cs3733.D21.teamF.database.ConnectionHandler;
 import edu.wpi.cs3733.D21.teamF.database.DatabaseAPI;
 import edu.wpi.cs3733.D21.teamF.entities.EdgeEntry;
 import edu.wpi.cs3733.D21.teamF.entities.NodeEntry;
+import edu.wpi.cs3733.D21.teamF.utils.CSVManager;
 import edu.wpi.cs3733.D21.teamF.utils.UIConstants;
 import edu.wpi.cs3733.uicomponents.MapPanel;
 import edu.wpi.cs3733.uicomponents.entities.DrawableEdge;
@@ -47,8 +48,8 @@ public class MapEditViewController {
     @FXML private TabPane tabPane;
     @FXML private Tab nodesTab;
     @FXML private Tab edgesTab;
-    @FXML private JFXTreeTableView nodeTreeTable;
-    @FXML private JFXTreeTableView edgeTreeTable;
+    @FXML private JFXTreeTableView<NodeEntry> nodeTreeTable;
+    @FXML private JFXTreeTableView<EdgeEntry> edgeTreeTable;
     @FXML private MapPanel mapPanel;
 
     private final ObservableList<EdgeEntry> edgeEntryObservableList = FXCollections.observableArrayList();
@@ -376,6 +377,109 @@ public class MapEditViewController {
         }
     }
 
+    public void handleSearch(KeyEvent keyEvent) {
+    }
+
+    public void handleNew(ActionEvent actionEvent) {
+    }
+
+    public void handleEdit(ActionEvent actionEvent) {
+    }
+
+    public void handleDelete(ActionEvent actionEvent) throws SQLException {
+        if(nodesTab.isSelected()) {
+            int selectedIndex = nodeTreeTable.getSelectionModel().getSelectedIndex(); // get index of table that is selected - KD
+            if (selectedIndex < 0) {
+                return;
+            }
+
+            String targetID = nodeTreeTable.getTreeItem(selectedIndex).getValue().getNodeID();
+            DatabaseAPI.getDatabaseAPI().deleteNode(targetID);
+            nodeEntryObservableList.remove(selectedIndex); // remove said index from table - KD
+            selectedCircle = null;
+            mapPanel.unDraw(targetID);
+        }
+
+        else if (edgesTab.isSelected()) {
+            int index = edgeTreeTable.getSelectionModel().getSelectedIndex();
+            EdgeEntry selectedEdge = null;
+
+            // Check for a valid index (-1 = no selection)
+            try{
+                // Remove the edge, this will update the TableView automatically
+                selectedEdge = edgeTreeTable.getTreeItem(index).getValue();
+                DatabaseAPI.getDatabaseAPI().deleteEdge(edgeEntryObservableList.get(index).getEdgeID());
+                edgeEntryObservableList.remove(index);
+                selectedLine = null;
+                mapPanel.unDraw(selectedEdge.getEdgeID());
+            } catch (ArrayIndexOutOfBoundsException e) {
+                // Create an alert to inform the user there is no edge selected
+                Alert alert = new Alert(Alert.AlertType.WARNING);
+                alert.initOwner(null); // Appears on top of all other windows
+                alert.setTitle("No Selection");
+                alert.setHeaderText("No Edge Selected");
+                alert.setContentText("Please select an edge from the list");
+                e.printStackTrace();
+                alert.showAndWait();
+            }
+        }
+    } //TODO test deleting NODE
+
+    public void handleSave(ActionEvent actionEvent) {
+    }
+
+    public void handleLoad(ActionEvent actionEvent) {
+    }
+
+    public void handleReset(ActionEvent actionEvent) throws SQLException {
+        mapPanel.clearMap();
+        nodeEntryObservableList.clear();
+        edgeEntryObservableList.clear();
+
+        List<String[]> nodeData = null;
+        List<String[]> edgeData = null;
+
+        try {
+            nodeData = (CSVManager.load("MapfAllNodes.csv"));
+            edgeData = (CSVManager.load("MapfAllEdges.csv"));
+        } catch (Exception e) {
+            e.printStackTrace();
+            return;
+        }
+
+        if(nodeData != null && edgeData != null)
+        {
+            if(!nodeData.isEmpty() && nodeData.get(0).length == 8 && !edgeData.isEmpty() && edgeData.get(0).length == 3 )
+            {
+                nodeEntryObservableList.addAll(nodeData.stream().map(line -> {
+                    return new NodeEntry(line[0], line[1], line[2], line[3], line[4], line[5], line[6], line[7]);
+                }).sorted(Comparator.comparing(NodeEntry::getNodeID)).collect(Collectors.toList()));
+                edgeEntryObservableList.addAll(edgeData.stream().map(line -> {
+                    return new EdgeEntry(line[0], line[1], line[2]);
+                }).sorted(Comparator.comparing(EdgeEntry::getEdgeID)).collect(Collectors.toList()));
+
+                DatabaseAPI.getDatabaseAPI().dropTable(ConnectionHandler.getConnection(), "L1NODES");
+                final String initNodesTable = "CREATE TABLE L1Nodes(NodeID varchar(200), " +
+                        "xCoord int, yCoord int, floor varchar(200), building varchar(200), " +
+                        "nodeType varchar(200), longName varchar(200), shortName varchar(200), primary key(NodeID))";
+                DatabaseAPI.getDatabaseAPI().createTable(ConnectionHandler.getConnection(), initNodesTable);
+                DatabaseAPI.getDatabaseAPI().populateNodes(nodeData); //NOTE: now can specify CSV arguments
+
+                DatabaseAPI.getDatabaseAPI().dropTable(ConnectionHandler.getConnection(), "L1EDGES");
+                final String initEdgesTable = "CREATE TABLE L1Edges(edgeID varchar(200), " +
+                        "startNode varchar(200), endNode varchar(200), primary key(edgeID))";
+                DatabaseAPI.getDatabaseAPI().createTable(ConnectionHandler.getConnection(), initEdgesTable);
+                DatabaseAPI.getDatabaseAPI().populateEdges(edgeData); //NOTE: now can specify CSV arguments
+
+                drawEdgeNodeOnFloor(); //FIXME do better with queries
+            }
+        }
+
+    }
+
+    public void handleHome(MouseEvent mouseEvent) {
+    }
+
     public void handleHoverOn(MouseEvent mouseEvent) {
         JFXButton btn = (JFXButton) mouseEvent.getSource();
         btn.setStyle("-fx-background-color: #F0C808; -fx-text-fill: #000000;");
@@ -386,27 +490,39 @@ public class MapEditViewController {
         btn.setStyle("-fx-background-color: #03256C; -fx-text-fill: #FFFFFF;");
     }
 
-    public void handleSearch(KeyEvent keyEvent) {
-    }
+    /**
+     * Select node based on selection in Table, focus on the node
+     * @author ZheCheng
+     */
+    public void handleSelectNode(MouseEvent mouseEvent) {
+        if(nodeTreeTable.getSelectionModel().getSelectedIndex() < 0){
+            // FIXME Error Handling
+            return;
+        }
+        // FIXME: ADD TRY_CATCH
+        NodeEntry node = nodeTreeTable.getSelectionModel().getSelectedItem().getValue(); //Changed for handling when the nodeTreeTable is being searched
 
-    public void handleNew(ActionEvent actionEvent) {
-    }
+        if(node == null){
+            //FIXME Null Warning
+            return;
+        }
 
-    public void handleEdit(ActionEvent actionEvent) {
-    }
-
-    public void handleDelete(ActionEvent actionEvent) {
-    }
-
-    public void handleSave(ActionEvent actionEvent) {
-    }
-
-    public void handleLoad(ActionEvent actionEvent) {
-    }
-
-    public void handleReset(ActionEvent actionEvent) {
-    }
-
-    public void handleHome(MouseEvent mouseEvent) {
-    }
+        // Check if need to switch map
+        if(node.getFloor().equals(mapPanel.getFloor().get())){
+            //mapPanel.drawNodeOnFloor();
+            if(selectedCircle != null)
+                selectedCircle.setFill(UIConstants.NODE_COLOR);
+        }else{
+            //floor = node.getFloor();
+            mapPanel.switchMap(node.getFloor());
+        }
+        Circle c = (Circle) mapPanel.getCanvas().lookup("#"+node.getNodeID());
+        if(c == null){
+            //FIXME Null Warning
+            return;
+        }
+        selectedCircle = c;
+        c.setFill(UIConstants.NODE_COLOR_SELECTED);
+        mapPanel.centerNode(c);
+    } //FIXME being weird
 }
