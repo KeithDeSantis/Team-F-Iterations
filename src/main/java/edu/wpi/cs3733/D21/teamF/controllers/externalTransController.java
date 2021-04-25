cf79d3b7c4692676db075ae3ca6e56b75bac3500
Jdominguez991 <73549619+Jdominguez991@users.noreply.github.com>
2021-04-25 13:36:19 -0400

    JD updated service home

---
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/externalTransController.java
@@ -0,0 +1,24 @@
+package edu.wpi.cs3733.D21.teamF.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+import javax.swing.*;
+
+public class externalTransController {
+    @FXML private JFXTextField employeeName;
+    @FXML private JFXTextField location;
+    @FXML private JFXTextField methodTrans;
+    @FXML private JFXTextField special;
+    @FXML private JFXButton submit;
+
+    public void goHome(MouseEvent mouseEvent) {
+    }
+
+    public void submitpushed(ActionEvent actionEvent) {
+    }
+}
