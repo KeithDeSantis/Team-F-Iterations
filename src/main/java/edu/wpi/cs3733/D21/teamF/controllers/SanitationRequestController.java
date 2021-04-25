bd14051db80c306811976bce36336233d799c857
Benstaw <ben@2broadview.com>
2021-04-25 15:17:10 -0400

    BS started sanitation service request

---
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/SanitationRequestController.java
@@ -0,0 +1,18 @@
+package edu.wpi.cs3733.D21.teamF.controllers;
+
+import com.jfoenix.controls.*;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class SanitationRequestController {
+
+    @FXML private JFXButton submit;
+    @FXML private JFXTextField jobDescription;
+    @FXML private JFXTextField roomLocated;
+    @FXML private JFXTextField employeeAssigned;
+
+    public void handleSubmit(ActionEvent actionEvent){
+
+    }
+
+}
