2f001ed81d03ffae269dba9b6a503c7e062c6b99
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2021-04-25 12:31:57 -0400

    Declan - working on serevice request

---
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/LaundryRequestController.java
@@ -0,0 +1,22 @@
+package edu.wpi.cs3733.D21.teamF.controllers;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+
+public class LaundryRequestController {
+
+    @FXML private Button submit;
+    @FXML private Button cancel;
+    public TextField employeeID;
+    public TextField clientName;
+
+    public void submitReq(ActionEvent actionEvent) {
+    }
+
+    public void cancelReq(ActionEvent actionEvent) {
+    }
+}
