8f102c9aef98f9c2a753c59650fba2ed8ca5394f
Jdominguez991 <73549619+Jdominguez991@users.noreply.github.com>
2021-04-24 20:40:07 -0400

    JD linked buttons to code

---
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/ServiceRequestHomeNewController.java
@@ -0,0 +1,15 @@
+package edu.wpi.cs3733.D21.teamF.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.scene.input.MouseEvent;
+
+public class ServiceRequestHomeNewController {
+    public void handleButtonPushed(ActionEvent actionEvent) {
+    }
+
+    public void handleHoverOn(MouseEvent mouseEvent) {
+    }
+
+    public void handleHoverOff(MouseEvent mouseEvent) {
+    }
+}
