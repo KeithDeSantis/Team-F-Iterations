e6e1776a22871b1a0abc4be6bb129201102d2925
Benstaw <ben@2broadview.com>
2021-04-26 11:46:09 -0400

    BS started sanitation service request

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/SanitationRequestController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/SanitationRequestController.java
@@ -1,17 +1,49 @@
 package edu.wpi.cs3733.D21.teamF.controllers;
 
 import com.jfoenix.controls.*;
+import edu.wpi.cs3733.D21.teamF.database.ConnectionHandler;
+import edu.wpi.cs3733.D21.teamF.database.DatabaseAPI;
+import edu.wpi.cs3733.D21.teamF.entities.EdgeEntry;
+import edu.wpi.cs3733.D21.teamF.entities.NodeEntry;
+import edu.wpi.cs3733.D21.teamF.pathfinding.Graph;
+import edu.wpi.cs3733.D21.teamF.pathfinding.GraphLoader;
+import edu.wpi.cs3733.D21.teamF.pathfinding.Vertex;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SanitationRequestController {
 
     @FXML private JFXButton submit;
-    @FXML private JFXTextField jobDescription;
-    @FXML private JFXTextField roomLocated;
-    @FXML private JFXTextField employeeAssigned;
+    @FXML private JFXTextArea description;
+    @FXML private JFXComboBox<String> location;
+    @FXML private JFXComboBox<String> employeeAssigned;
+
+
+    private void initialize(){
+        Graph graph = new Graph();
+        try {
+            List<NodeEntry> nodeEntries = DatabaseAPI.getDatabaseAPI().genNodeEntries(ConnectionHandler.getConnection());
+
+            final ObservableList<String> nodeList = FXCollections.observableArrayList();
+            nodeList.addAll(nodeEntries.stream().map(NodeEntry::getShortName)
+                    .sorted().collect(Collectors.toList()));
+            this.location.setItems(nodeList);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            //return;
+        }
+
+    }
 
     public void handleSubmit(ActionEvent actionEvent){
+
+
 
     }
 
