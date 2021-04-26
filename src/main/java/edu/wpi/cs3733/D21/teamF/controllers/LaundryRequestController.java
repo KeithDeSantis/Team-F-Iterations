569fb0a059784095a079010469620f4b659e256e
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2021-04-26 11:06:46 -0400

    Declan - more service request work

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/LaundryRequestController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/LaundryRequestController.java
@@ -4,6 +4,7 @@ import com.jfoenix.controls.JFXButton;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 
 
@@ -11,12 +12,22 @@ public class LaundryRequestController {
 
     @FXML private Button submit;
     @FXML private Button cancel;
+    @FXML private Button help;
+    @FXML private RadioButton darks;
+    @FXML private RadioButton lights;
+    @FXML private RadioButton both;
+    @FXML private RadioButton hot;
+    @FXML private RadioButton cold;
+    @FXML private RadioButton folded;
     public TextField employeeID;
     public TextField clientName;
+    public TextField additionalInstructions;
 
+    @FXML
     public void submitReq(ActionEvent actionEvent) {
     }
 
+    @FXML
     public void cancelReq(ActionEvent actionEvent) {
     }
 }
