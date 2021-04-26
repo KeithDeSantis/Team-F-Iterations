f25a3fb9c857048bdc2df8c7cc8ce1448bbbdc71
Benstaw <ben@2broadview.com>
2021-04-26 18:21:59 -0400

    BS  finished basic sanitation request

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/SanitationRequestController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/SanitationRequestController.java
@@ -5,14 +5,19 @@ import edu.wpi.cs3733.D21.teamF.database.ConnectionHandler;
 import edu.wpi.cs3733.D21.teamF.database.DatabaseAPI;
 import edu.wpi.cs3733.D21.teamF.entities.NodeEntry;
 
-import edu.wpi.cs3733.D21.teamF.pathfinding.Graph;
-import edu.wpi.cs3733.D21.teamF.pathfinding.GraphLoader;
-import edu.wpi.cs3733.D21.teamF.pathfinding.Vertex;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.embed.swing.JFXPanel;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,12 +31,9 @@ public class SanitationRequestController {
 
 
     private void initialize(){
-        Graph graph = new Graph();
         try {
             List<NodeEntry> nodeEntries = DatabaseAPI.getDatabaseAPI().genNodeEntries(ConnectionHandler.getConnection());
         //    List<UserEntry> UserEntries = DatabaseAPI.getDatabaseAPI().genNodeEntries(ConnectionHandler.getConnection());
-
-
 
             final ObservableList<String> nodeList = FXCollections.observableArrayList();
             nodeList.addAll(nodeEntries.stream().map(NodeEntry::getShortName)
@@ -40,15 +42,29 @@ public class SanitationRequestController {
 
         } catch (Exception e) {
             e.printStackTrace();
-            //return;
         }
 
     }
 
-    public void handleSubmit(ActionEvent actionEvent){
+    public void handleSubmit(ActionEvent actionEvent) throws IOException{
+        Stage submittedStage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D21/teamF/fxml/FormSubmittedView.fxml")); // Loading in pop up View
+        Scene submitScene = new Scene(root);
+        submittedStage.setScene(submitScene);
+        submittedStage.setTitle("Submission Complete");
+        submittedStage.initModality(Modality.APPLICATION_MODAL);
+        submittedStage.initOwner(((Button)actionEvent.getSource()).getScene().getWindow());
+        submittedStage.showAndWait();
+    }
 
-
-
+    public void handleCancel(ActionEvent actionEvent) throws IOException {
+        Stage stage;
+        Parent root;
+        stage = (Stage) cancel.getScene().getWindow();
+        root = FXMLLoader.load(getClass().getResource("/edu/wpi/cs3733/D21/teamF/fxml/ServiceRequestHomeNewView.fxml"));
+        stage.getScene().setRoot(root);
+        stage.setTitle("Service Request Home");
+        stage.show();
     }
 
 }
