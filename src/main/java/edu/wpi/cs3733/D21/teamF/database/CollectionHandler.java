1f3a03139077b0b2feb7662e62eed3ef0a050a43
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2021-05-02 15:51:24 -0400

    Declan - javadoc

--- src/main/java/edu/wpi/cs3733/D21/teamF/database/CollectionHandler.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/database/CollectionHandler.java
@@ -7,6 +7,13 @@ import java.sql.Statement;
 import java.util.ArrayList;
 
 public class CollectionHandler {
+    /**
+     * Returns a list of a user's favorite or recently used nodes
+     * @param type either favorite or recent for node filtering
+     * @param id the id of the user (username)
+     * @return list of strings for node ids
+     * @throws SQLException on error with DB operations
+     */
     public ArrayList<String> getUserNodes(String type, String id) throws SQLException{
         final String query = "SELECT * FROM COLLECTIONS";
         ResultSet rset;
@@ -25,6 +32,14 @@ public class CollectionHandler {
         return userNodes;
     }
 
+    /**
+     * adds a node entry to the collections table
+     * @param user the user associated with the node
+     * @param node the nodeid to insert
+     * @param type the type either favorite or recent
+     * @return true on success, false otherwise
+     * @throws SQLException on error with DB operations
+     */
     public boolean addEntry(String user, String node, String type) throws SQLException{
         final String query = "INSERT INTO COLLECTIONS values(DEFAULT, ?, ?, ?)";
         PreparedStatement stmt = ConnectionHandler.getConnection().prepareStatement(query);
@@ -34,6 +49,10 @@ public class CollectionHandler {
         return stmt.executeUpdate() != 0;
     }
 
+    /**
+     * drops the collections table
+     * @return true on success false otherwise
+     */
     public boolean dropCollectionsTable(){
         boolean success = false;
         String query = "DROP TABLE COLLECTIONS";
@@ -49,6 +68,10 @@ public class CollectionHandler {
         return success;
     }
 
+    /**
+     * create the collections table
+     * @return true on success false otherwise
+     */
     public boolean createCollectionTable(){
         boolean success = false;
         final String initNodesTable = "CREATE TABLE COLLECTIONS(id int GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1), " +
