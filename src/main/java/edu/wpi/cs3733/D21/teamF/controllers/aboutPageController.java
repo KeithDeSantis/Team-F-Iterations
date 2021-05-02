7a2b25a7d0a06af157c18c2a843be803bebfe927
Jdominguez991 <73549619+Jdominguez991@users.noreply.github.com>
2021-05-02 13:18:18 -0400

    JD-about button navigation

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/aboutPageController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/aboutPageController.java
@@ -1,7 +1,6 @@
 package edu.wpi.cs3733.D21.teamF.controllers;
 
 import com.jfoenix.controls.JFXButton;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 import edu.wpi.cs3733.D21.teamF.utils.SceneContext;
 import javafx.fxml.FXML;
 
@@ -11,7 +10,7 @@ import java.io.IOException;
 public class aboutPageController {
     @FXML JFXButton back;
     public void goBack(ActionEvent actionevent) throws IOException {
-        SceneContext.getSceneContext().switchScene("/edu/wpi/cs3733/D21/teamF/fxml/PreferredPathfindingAlgoView");
+        SceneContext.getSceneContext().switchScene("/edu/wpi/cs3733/D21/teamF/fxml/AStarDemoView.fxml");
     }
 
 
