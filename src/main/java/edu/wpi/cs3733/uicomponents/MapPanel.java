ec2da4273af8c464db2386ab66e1779c3fb4d93e
Tony Vuolo <vuolo.969473@gmail.com>
2021-05-06 19:20:20 -0400

    Tony Vuolo - added "this" to object fields in MapPanel.java

--- src/main/java/edu/wpi/cs3733/uicomponents/MapPanel.java
+++ src/main/java/edu/wpi/cs3733/uicomponents/MapPanel.java
@@ -136,40 +136,42 @@ public class MapPanel extends AnchorPane {
 
     @FXML
     public void initialize(){
-        zoomInButton.setOnAction(this::handleZoom);
-        zoomOutButton.setOnAction(this::handleZoom);
+        this.zoomInButton.setOnAction(this::handleZoom);
+        this.zoomOutButton.setOnAction(this::handleZoom);
 
         // Set button fonts - LM
-        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        map.setPreserveRatio(true);
+        this.scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        this.scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        this.map.setPreserveRatio(true);
 
 
-        INITIAL_WIDTH.setValue(F1Image.getWidth());
-        INITIAL_HEIGHT.setValue(F1Image.getHeight());
+        INITIAL_WIDTH.setValue(this.F1Image.getWidth());
+        INITIAL_HEIGHT.setValue(this.F1Image.getHeight());
 
 
-        stackPane.prefWidthProperty().bind(this.widthProperty());
-        stackPane.prefHeightProperty().bind(this.heightProperty());
+        this.stackPane.prefWidthProperty().bind(this.widthProperty());
+        this.stackPane.prefHeightProperty().bind(this.heightProperty());
 
 
-        canvas.prefWidthProperty().bind(INITIAL_WIDTH.divide(zoomLevel));
-        canvas.prefHeightProperty().bind(INITIAL_HEIGHT.divide(zoomLevel));
+        this.canvas.prefWidthProperty().bind(INITIAL_WIDTH.divide(this.zoomLevel));
+        this.canvas.prefHeightProperty().bind(INITIAL_HEIGHT.divide(this.zoomLevel));
 
-        map.fitWidthProperty().bind(INITIAL_WIDTH.divide(zoomLevel));
-        map.fitHeightProperty().bind(INITIAL_HEIGHT.divide(zoomLevel));
-        map.setImage(F1Image); // Copied from A* Vis - KD
+        this.map.fitWidthProperty().bind(INITIAL_WIDTH.divide(this.zoomLevel));
+        this.map.fitHeightProperty().bind(INITIAL_HEIGHT.divide(this.zoomLevel));
+        this.map.setImage(this.F1Image); // Copied from A* Vis - KD
 
-        floorSlider.setLabelFormatter(doubleStringConverter);
+        this.floorSlider.setLabelFormatter(this.doubleStringConverter);
 
        // final StringBinding binding =  Bindings.createStringBinding(() -> floorSlider.getLabelFormatter().toString(floorSlider.valueProperty().get()), floorSlider.valueProperty());
 
 
-        Bindings.bindBidirectional(this.floor, floorSlider.valueProperty(), doublePropertyStringConverter);
+        Bindings.bindBidirectional(this.floor, this.floorSlider.valueProperty(), this.doublePropertyStringConverter);
 
         this.floorSlider.valueProperty().addListener(e -> switchMap(this.doubleStringConverter.toString(this.floorSlider.valueProperty().get())));
 
-        fp.bind(this.floor);
+        this.fp.bind(this.floor);
+
+        this.scroll.pannableProperty().set(true);
     }
 
 
@@ -189,19 +191,19 @@ public class MapPanel extends AnchorPane {
         this.floor.setValue(floor);
         switch(floor){
             case "1":
-                map.setImage(F1Image); break;
+                this.map.setImage(this.F1Image); break;
             case "2":
-                map.setImage(F2Image); break;
+                this.map.setImage(this.F2Image); break;
             case "3":
-                map.setImage(F3Image); break;
+                this.map.setImage(this.F3Image); break;
             case "L1":
-                map.setImage(L1Image); break;
+                this.map.setImage(this.L1Image); break;
             case "L2":
-                map.setImage(L2Image); break;
+                this.map.setImage(this.L2Image); break;
             case "G":
-                map.setImage(GImage); break;
+                this.map.setImage(this.GImage); break;
             default:
-                map.setImage(F1Image); System.out.println("No Such Floor!"); break; //FIXME : Error Handling
+                this.map.setImage(this.F1Image); System.out.println("No Such Floor!"); break; //FIXME : Error Handling
         }
         //drawNodeOnFloor();
     }
@@ -209,12 +211,12 @@ public class MapPanel extends AnchorPane {
 
 
     public ImageView getMap() {
-        return map;
+        return this.map;
     }
 
 
     public DoubleProperty getZoomLevel() {
-        return zoomLevel;
+        return this.zoomLevel;
     }
 
     public ObjectProperty<String> getFloor() {
@@ -222,7 +224,7 @@ public class MapPanel extends AnchorPane {
     }
 
     public Pane getCanvas() {
-        return canvas;
+        return this.canvas;
     }
 
 
@@ -233,18 +235,18 @@ public class MapPanel extends AnchorPane {
      */
     public void handleZoom(ActionEvent actionEvent) { //TODO Fix Centering so centering node works when zoom level is changed
         JFXButton btn = (JFXButton) actionEvent.getSource();
-        if(btn == zoomInButton) {
-            if(zoomLevel.get() > 1) {
-                zoomLevel.setValue(zoomLevel.get()  - 1);
+        if(btn == this.zoomInButton) {
+            if(this.zoomLevel.get() > 1) {
+                this.zoomLevel.setValue(this.zoomLevel.get()  - 1);
             }
-        } else if (btn == zoomOutButton) {
-            if(zoomLevel.get() < 8) {
-                zoomLevel.setValue(zoomLevel.get() + 1);
+        } else if (btn == this.zoomOutButton) {
+            if(this.zoomLevel.get() < 8) {
+                this.zoomLevel.setValue(this.zoomLevel.get() + 1);
             }
         }
 
-        Image image = map.getImage();
-        map.setImage(image);
+        Image image = this.map.getImage();
+        this.map.setImage(image);
     }
 
     /**
@@ -254,17 +256,17 @@ public class MapPanel extends AnchorPane {
      */
     public void centerNode(Node node){
 
-        double h = scroll.getContent().getBoundsInLocal().getHeight();
+        double h = this.scroll.getContent().getBoundsInLocal().getHeight();
         double y = (node.getBoundsInParent().getMaxY() +
                 node.getBoundsInParent().getMinY()) / 2.0;
-        double v = scroll.getViewportBounds().getHeight();
-        scroll.setVvalue(scroll.getVmax() * ((y - 0.5 * v) / (h - v)));
+        double v = this.scroll.getViewportBounds().getHeight();
+        this.scroll.setVvalue(this.scroll.getVmax() * ((y - 0.5 * v) / (h - v)));
 
-        double w = scroll.getContent().getBoundsInLocal().getWidth();
+        double w = this.scroll.getContent().getBoundsInLocal().getWidth();
         double x = (node.getBoundsInParent().getMaxX() +
                 node.getBoundsInParent().getMinX()) / 2.0;
-        double hw = scroll.getViewportBounds().getWidth();
-        scroll.setHvalue(scroll.getHmax() * -((x - 0.5 * hw) / (hw - w)));
+        double hw = this.scroll.getViewportBounds().getWidth();
+        this.scroll.setHvalue(this.scroll.getHmax() * -((x - 0.5 * hw) / (hw - w)));
     }
 
     /**
