428e03204d66fd2a1b84760b76c4ab8ba5d4f149
af <ahftesla@outlook.com>
2021-05-07 15:54:27 -0400

    preliminary loading

--- src/main/java/edu/wpi/cs3733/D21/teamF/utils/SceneContext.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/utils/SceneContext.java
@@ -1,12 +1,13 @@
 package edu.wpi.cs3733.D21.teamF.utils;
 
 import edu.wpi.cs3733.D21.teamF.controllers.AbsController;
-import javafx.application.Platform;
+import javafx.animation.PauseTransition;
 import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 import java.io.IOException;
 import java.util.concurrent.ExecutionException;
@@ -27,6 +28,14 @@ public class SceneContext {
         return SceneContextSingletonHelper.sceneContext;
     }
 
+    private void showLoadingPage() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("/edu/wpi/cs3733/D21/teamF/fxml/LoadPage.fxml"));
+        controller = loader.getController();
+        stage.setScene(new Scene(loader.load()));
+        stage.show();
+    }
+
     public void switchScene(String fxml) throws IOException {
         final Task<Parent> task = new Task<Parent>() {
             @Override
@@ -44,6 +53,21 @@ public class SceneContext {
             } catch (InterruptedException | ExecutionException interruptedException) {
                 interruptedException.printStackTrace();
             }
+        });
+
+        task.setOnScheduled( e -> {
+            final PauseTransition timeOut = new PauseTransition(Duration.seconds(0.2));
+            timeOut.setOnFinished((timeout) -> {
+                if(task.isRunning()) {
+                    try {
+                        showLoadingPage();
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
+                    }
+                }
+            });
+            timeOut.play();
+
         });
 
         Thread t = new Thread(task);
