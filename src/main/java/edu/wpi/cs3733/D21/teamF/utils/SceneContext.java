61bdeebc98ca61e26a51a9f5a5f3cd72712fa4ae
af <ahftesla@outlook.com>
2021-05-07 15:23:54 -0400

    attemping to inline

--- src/main/java/edu/wpi/cs3733/D21/teamF/utils/SceneContext.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/utils/SceneContext.java
@@ -1,12 +1,15 @@
 package edu.wpi.cs3733.D21.teamF.utils;
 
 import edu.wpi.cs3733.D21.teamF.controllers.AbsController;
+import javafx.application.Platform;
+import javafx.concurrent.Task;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.concurrent.ExecutionException;
 
 public class SceneContext {
 
@@ -25,23 +28,26 @@ public class SceneContext {
     }
 
     public void switchScene(String fxml) throws IOException {
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(getClass().getResource(fxml));
-        this.controller = loader.getController();
-        final Thread thread = new Thread(() -> {
+        final Task<Parent> task = new Task<Parent>() {
+            @Override
+            public Parent call() throws IOException {
+                FXMLLoader loader = new FXMLLoader();
+                loader.setLocation(getClass().getResource(fxml));
+                controller = loader.getController();
+                return loader.load();
+            }
+        };
+        task.setOnSucceeded( e-> {
             try {
-                final Parent root = loader.load();
-                stage.setScene(new Scene(root));
+                stage.setScene(new Scene(task.get()));
                 stage.show();
-            } catch (IOException e) {
-                e.printStackTrace();
+            } catch (InterruptedException | ExecutionException interruptedException) {
+                interruptedException.printStackTrace();
             }
         });
 
-        thread.start();
-//        Parent root = loader.load();
-//        stage.setScene(new Scene(root));
-//        stage.show();
+        Thread t = new Thread(task);
+        t.start();
     }
 
     public void loadDefault() throws IOException {
