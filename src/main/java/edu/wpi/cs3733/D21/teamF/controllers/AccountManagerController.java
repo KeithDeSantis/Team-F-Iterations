85c55ad84824f103924e6da58d1c5a4d4fdacf9c
keithdesantis <kwdesantis@wpi.edu>
2021-05-07 15:59:31 -0400

    KD - working on it

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/AccountManagerController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/AccountManagerController.java
@@ -3,17 +3,24 @@ package edu.wpi.cs3733.D21.teamF.controllers;
 import com.jfoenix.controls.*;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 import edu.wpi.cs3733.D21.teamF.database.DatabaseAPI;
+import edu.wpi.cs3733.D21.teamF.database.UserHandler;
 import edu.wpi.cs3733.D21.teamF.entities.AccountEntry;
 import edu.wpi.cs3733.D21.teamF.utils.SceneContext;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeTableColumn;
+import javafx.scene.control.cell.ComboBoxTreeTableCell;
+import javafx.scene.control.cell.TextFieldTreeTableCell;
 
 import java.io.IOException;
 import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.SQLException;
 import java.util.List;
 import java.util.ResourceBundle;
@@ -26,48 +33,79 @@ public class AccountManagerController extends AbsController implements Initializ
     @FXML
     private JFXButton addUser;
     @FXML
-    private JFXButton saveChanges;
-    @FXML
     private JFXButton home;
-    @FXML
-    private JFXComboBox<String> selectUser;
-    @FXML
-    private JFXComboBox<String> changeUserType;
-    @FXML
-    private JFXComboBox<String> newUserType;
-    @FXML
-    private JFXTextField username;
-    @FXML
-    private JFXTextField password;
-    @FXML
-    private JFXTextField addPassword;
-    @FXML
-    private JFXTextField addUsername;
 
-    //private String fieldChanged = "";
     @FXML
     private JFXTreeTableView<AccountEntry> accountView;
     private final ObservableList<AccountEntry> accounts = FXCollections.observableArrayList();
 
     public void initialize(URL location, ResourceBundle resources) {
 
-        int colWidth = 430;
+        int colWidth = 286;
         JFXTreeTableColumn<AccountEntry, String> username = new JFXTreeTableColumn<>("Username");
         username.setPrefWidth(colWidth);
         username.setCellValueFactory(cellData -> cellData.getValue().getValue().getUsernameProperty());
 
-//        JFXTreeTableColumn<AccountEntry, String> password = new JFXTreeTableColumn<>("Password");
-//        password.setPrefWidth(colWidth);
-//        password.setCellValueFactory(cellData -> cellData.getValue().getValue().getPasswordProperty());
+        username.setCellFactory(TextFieldTreeTableCell.forTreeTableColumn());
+        username.setOnEditCommit(new EventHandler<TreeTableColumn.CellEditEvent<AccountEntry, String>>() {
+            @Override
+            public void handle(TreeTableColumn.CellEditEvent<AccountEntry, String> event) {
+                TreeItem<AccountEntry> selectedAccount = accountView.getTreeItem(event.getTreeTablePosition().getRow());
+                try {
+                    DatabaseAPI.getDatabaseAPI().editUser(selectedAccount.getValue().getUsername(), event.getNewValue(), "username");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                selectedAccount.getValue().setUsername(event.getNewValue());
+            }
+        });
 
+        JFXTreeTableColumn<AccountEntry, String> password = new JFXTreeTableColumn<>("Password");
+        password.setPrefWidth(colWidth);
+        password.setCellValueFactory(cellData -> cellData.getValue().getValue().getPasswordProperty());
+
+        password.setCellFactory(TextFieldTreeTableCell.forTreeTableColumn());
+        password.setOnEditCommit(new EventHandler<TreeTableColumn.CellEditEvent<AccountEntry, String>>() {
+            @Override
+            public void handle(TreeTableColumn.CellEditEvent<AccountEntry, String> event) {
+                TreeItem<AccountEntry> selectedAccount = accountView.getTreeItem(event.getTreeTablePosition().getRow());
+                try {
+                    DatabaseAPI.getDatabaseAPI().editUser(selectedAccount.getValue().getUsername(), "", "password");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                selectedAccount.getValue().setUsername(event.getNewValue());
+            }
+        });
+
+        ObservableList<String> typeList = FXCollections.observableArrayList();
+        typeList.add("administrator");
+        typeList.add("employee");
+        typeList.add("visitor");
         JFXTreeTableColumn<AccountEntry, String> userType = new JFXTreeTableColumn<>("User Type");
         userType.setPrefWidth(colWidth);
         userType.setCellValueFactory(cellData -> cellData.getValue().getValue().getUserTypeProperty());
 
+
+        userType.setCellFactory(ComboBoxTreeTableCell.forTreeTableColumn(typeList));
+        //userType.setCellValueFactory(cellData -> cellData.getValue().getValue().getUserTypeProperty());
+        userType.setOnEditCommit(new EventHandler<TreeTableColumn.CellEditEvent<AccountEntry, String>>() {
+            @Override
+            public void handle(TreeTableColumn.CellEditEvent<AccountEntry, String> event) {
+                TreeItem<AccountEntry> selectedAccount = accountView.getTreeItem(event.getTreeTablePosition().getRow());
+                try {
+                    DatabaseAPI.getDatabaseAPI().editUser(selectedAccount.getValue().getUsername(), event.getNewValue(), "type");
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                selectedAccount.getValue().setUserType(event.getNewValue());
+            }
+        });
+
         final TreeItem<AccountEntry> root = new RecursiveTreeItem<>(accounts, RecursiveTreeObject::getChildren);
         accountView.setRoot(root);
         accountView.setShowRoot(false);
-        accountView.getColumns().setAll(username, userType);
+        accountView.getColumns().setAll(username, password, userType);
 
         List<AccountEntry> data;
         try {
@@ -81,36 +119,7 @@ public class AccountManagerController extends AbsController implements Initializ
             e.printStackTrace();
         }
 
-
-
-        //add table entries like in account manager
-        //syntax of adding item: services.add(new ServiceEntry("Request Type", "Assigned To", "Status));
-
-//        List<String> allUsers;
-//        try {
-//            UserHandler userHandler = new UserHandler();
-//            allUsers = userHandler.listAllUsers();
-//            for (String s : allUsers)
-//            {
-//                selectUser.getItems().add(s);
-//            }
-//        }
-//        catch (SQLException e)
-//        {
-//            e.printStackTrace();
-//        }
-
-        changeUserType.getItems().add("guest");
-        changeUserType.getItems().add("employee");
-        changeUserType.getItems().add("admin");
-
-        newUserType.getItems().add("guest");
-        newUserType.getItems().add("employee");
-        newUserType.getItems().add("admin");
-    }
-
-
-    public void handleUserSearch(ActionEvent actionEvent) {
+        accountView.setEditable(true);
     }
 
     public void handleButtonPushed(ActionEvent actionEvent) throws Exception {
@@ -121,7 +130,6 @@ public class AccountManagerController extends AbsController implements Initializ
         else if (buttonPushed == deleteUser && accountView.getSelectionModel().getSelectedIndex() >= 0){
             AccountEntry user = accountView.getSelectionModel().getSelectedItem().getValue();
             DatabaseAPI.getDatabaseAPI().deleteUser(user.getUsername());
-            refreshPage();
         }
         else if (buttonPushed == addUser && !addUsername.getText().isEmpty() && !addPassword.getText().isEmpty() && !(newUserType.getValue()==null)){
 
@@ -134,49 +142,6 @@ public class AccountManagerController extends AbsController implements Initializ
             accounts.add(newUser);
             refreshPage();
         }
-        else if (buttonPushed == saveChanges && accountView.getSelectionModel().getSelectedIndex() >= 0){
-
-            String  targetUser = accountView.getSelectionModel().getSelectedItem().getValue().getUsername();
-            String newVal;
-
-            if(!username.getText().isEmpty()) {
-                newVal = username.getText();
-                DatabaseAPI.getDatabaseAPI().editUser(targetUser, newVal, "username");
-                accountView.getSelectionModel().getSelectedItem().getValue().setUsername(newVal);
-            }
-            if(!password.getText().isEmpty()) {
-                newVal = password.getText();
-                DatabaseAPI.getDatabaseAPI().editUser(targetUser, newVal, "password");
-                accountView.getSelectionModel().getSelectedItem().getValue().setPassword(newVal);
-            }
-            if(!(changeUserType.getValue() == null)) {
-                newVal = changeUserType.getValue();
-                DatabaseAPI.getDatabaseAPI().editUser(targetUser, newVal, "type");
-                accountView.getSelectionModel().getSelectedItem().getValue().setUserType(newVal);
-            }
-
-            /*
-            switch (fieldChanged) {
-                case "username":
-                    newVal = username.getText();
-                    DatabaseAPI.getDatabaseAPI().editUser(targetUser, newVal, "username");
-                    accountView.getSelectionModel().getSelectedItem().getValue().setUsername(newVal);
-                    break;
-                case "password":
-                    newVal = password.getText();
-                    DatabaseAPI.getDatabaseAPI().editUser(targetUser, newVal, "password");
-                    accountView.getSelectionModel().getSelectedItem().getValue().setPassword(newVal);
-                    break;
-                case "type":
-                    newVal = changeUserType.getValue();
-                    DatabaseAPI.getDatabaseAPI().editUser(targetUser, newVal, "type");
-                    accountView.getSelectionModel().getSelectedItem().getValue().setUserType(newVal);
-                    break;
-            }
-            fieldChanged = "";
-             */
-            refreshPage();
-        }
         else if (buttonPushed == home){
             SceneContext.getSceneContext().loadDefault();
         }
@@ -184,19 +149,6 @@ public class AccountManagerController extends AbsController implements Initializ
 
     private void refreshPage() throws IOException {
         SceneContext.getSceneContext().switchScene("/edu/wpi/cs3733/D21/teamF/fxml/AccountManagerView.fxml");
-    }
-
-
-    public void changingUsername() {
-        //fieldChanged = "username";
-    }
-
-    public void changingPassword() {
-        //fieldChanged = "password";
-    }
-
-    public void changingUserType() {
-        //fieldChanged = "type";
     }
 
 }
