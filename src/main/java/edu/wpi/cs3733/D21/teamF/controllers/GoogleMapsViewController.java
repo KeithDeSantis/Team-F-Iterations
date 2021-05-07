f3a6fed9a5f0c1df669858fd3a3a935c5d99fc07
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2021-05-07 17:17:39 -0400

    Declan - adding state abreviation stuff

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/GoogleMapsViewController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/GoogleMapsViewController.java
@@ -67,6 +67,11 @@ public class GoogleMapsViewController {
 
     @FXML
     public void initialize(){
-
+        String[] stateAbbreviations = {"AK","AL","AR","AZ","CA","CO","CT","DE","FL","GA","HI","IA","ID","IL","IN","KS",
+                "KY","LA","MA","MD","ME","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY","OH","OK","OR",
+                "PA","RI","SC","SD","TN","TX","UT","VA","VT","WA","WI","WV","WY"};
+        for (String s : stateAbbreviations){
+            state.getItems().add(s);
+        }
     }
 }
