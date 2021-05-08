ba7e2e2d71f97761bb6caf4635cb3c21c2e5f15c
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2021-05-08 19:52:22 -0400

    Declan - finding closest parking lot parsing

--- src/main/java/edu/wpi/cs3733/D21/teamF/pathfinding/GoogleDriverCode.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/pathfinding/GoogleDriverCode.java
@@ -6,7 +6,10 @@ import java.nio.charset.StandardCharsets;
 
 public class GoogleDriverCode {
     public static void main(String[] args) throws IOException {
-        String data = GoogleAPI.getGoogleAPI().queryAPI("997 Main St. Norwell, MA 02061", "BWH Visitor Parking, Francis Street, Boston, MA");
-        System.out.println(data);
+        String[] data = GoogleAPI.getGoogleAPI().queryAPI("997 Main St. Norwell, MA 02061", "BWH Visitor Parking, Francis Street, Boston, MA");
+        System.out.println(data[0]);
+        System.out.println(data[1]);
+        String address = GoogleAPI.getGoogleAPI().parseClosestParkingLot("997 Main St. Norwell MA, 02061");
+        System.out.println("Closest parking lot " + address);
     }
 }
