8f76bf5601d3e007ef233e8cd81a8d2c5db1edad
af <ahftesla@outlook.com>
2021-05-08 12:41:23 -0400

    changed DrawableUSer to be groupBased

--- src/main/java/edu/wpi/cs3733/uicomponents/entities/DrawableUser.java
+++ src/main/java/edu/wpi/cs3733/uicomponents/entities/DrawableUser.java
@@ -2,16 +2,19 @@ package edu.wpi.cs3733.uicomponents.entities;
 
 import edu.wpi.cs3733.uicomponents.IMapDrawable;
 import javafx.beans.property.*;
+import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
-public class DrawableUser extends Circle implements IMapDrawable
+public class DrawableUser extends Group implements IMapDrawable
 {
     private final IntegerProperty xCoordinate;
     private final IntegerProperty yCoordinate;
     private final StringProperty floor;
 
     private final SimpleBooleanProperty shouldDisplay;
+
+    final Circle user;
 
     public DrawableUser(int xCoordinate, int yCoordinate, String ID, String floor)
     {
@@ -24,15 +27,24 @@ public class DrawableUser extends Circle implements IMapDrawable
 
         this.setId(ID);
 
-        this.setRadius(10);
+        user = new Circle();
+        user.setRadius(10);
+        ///this.setRadius(10);
 
-        this.setFill(Color.CYAN);
+        //this.setFill(Color.CYAN);
+        user.setFill(Color.CYAN);
+
+        final Circle direction = new Circle();
+
+        this.getChildren().add(user);
+        //this.getCh
     }
 
     @Override
     public void bindLocation(DoubleProperty zoomLevel) {
-        this.centerXProperty().bind(xCoordinate.divide(zoomLevel));
-        this.centerYProperty().bind(yCoordinate.divide(zoomLevel));
+
+        user.centerXProperty().bind(xCoordinate.divide(zoomLevel));
+        user.centerYProperty().bind(yCoordinate.divide(zoomLevel));
     }
 
     @Override
