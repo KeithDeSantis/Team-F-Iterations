3fd7f8988e5747028876149a86049ba2d46e7e7b
af <ahftesla@outlook.com>
2021-05-09 09:05:41 -0400

    fixed high level controllers

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/ServiceRequestsControllers/GiftDeliveryServiceRequestController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/ServiceRequestsControllers/GiftDeliveryServiceRequestController.java
@@ -4,10 +4,7 @@ import com.jfoenix.controls.*;
 import edu.wpi.cs3733.D21.teamF.Translation.Translator;
 import edu.wpi.cs3733.D21.teamF.controllers.ServiceRequests;
 import edu.wpi.cs3733.D21.teamF.database.DatabaseAPI;
-import edu.wpi.cs3733.D21.teamF.entities.NodeEntry;
 import edu.wpi.cs3733.D21.teamF.utils.SceneContext;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -15,7 +12,6 @@ import javafx.scene.control.Label;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 public class GiftDeliveryServiceRequestController extends ServiceRequests {
