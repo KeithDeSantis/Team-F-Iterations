1b8c13b9f37828ef4b634a1de2fff2782da1e527
keithdesantis <kwdesantis@wpi.edu>
2021-05-11 13:41:13 -0400

    KD -  merged ahf-addMoreTrans down

--- src/main/java/edu/wpi/cs3733/D21/teamF/controllers/AStarDemoController.java
+++ src/main/java/edu/wpi/cs3733/D21/teamF/controllers/AStarDemoController.java
@@ -24,6 +24,7 @@ import javafx.beans.binding.IntegerBinding;
 import javafx.beans.binding.ObjectBinding;
 import javafx.beans.property.*;
 import javafx.collections.FXCollections;
+import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
@@ -112,6 +113,9 @@ public class AStarDemoController extends AbsController implements Initializable 
 
     @FXML
     private JFXTreeView<String> instructionTreeView;
+    
+    @FXML
+    private JFXNodesList stopList;
 
     //FIXME: DO BETTER
     private Graph graph;
@@ -534,8 +538,6 @@ public class AStarDemoController extends AbsController implements Initializable 
         // Set the root item
         treeView.setRoot(rootTreeViewItem);
 
-        System.out.println("TREEEE: " + treeView.getRoot().getChildren().get(0));
-
         // Hide root item (we don't need it visible, we always want to list to be there
         this.treeView.setShowRoot(false);
 
@@ -603,6 +605,24 @@ public class AStarDemoController extends AbsController implements Initializable 
         } catch (SQLException exception) {
             exception.printStackTrace();
         }
+        
+        /*
+         * Initialize the stop list
+         */
+        vertices.addListener((ListChangeListener<Vertex>) c -> {
+            while(c.next()){} //Needed to get all changes
+            stopList.getChildren().clear();
+            for (Vertex stop : vertices)
+            {
+                System.out.println(stop.getID());
+                final Label stopLbl = new Label(stop.getID());
+                stopList.addAnimatedNode(stopLbl);
+                stopList.getListAnimation(true);
+                stopList.animateList();
+                stopList.animateList(true); // this confuses me so much but it works - KD
+            }
+            System.out.println("---------");
+        });
     }
 
     private EventHandler<ActionEvent> handleWhatsHereMenu(NodeEntry currEntry, int i) {
@@ -711,8 +731,8 @@ public class AStarDemoController extends AbsController implements Initializable 
                 }
             } else {
                 vertices.remove(graph.getVertex(currEntry.getNodeID()));
-                mapPanel.unDraw(currEntry.getNodeID());
-                getDrawableNode(currEntry.getNodeID());
+//                mapPanel.unDraw(currEntry.getNodeID());
+//                getDrawableNode(currEntry.getNodeID());
             }
             checkInput();
         };
